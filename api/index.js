59a63211a60abbe2439458a2f9bf0c1ff6268060
Naman Nayal <nayalnaman@gmail.com>
2025-02-26 21:45:58 +0530

    connect to the db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import { log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO)
+.then(
+    ()=>{console.log('connection established')}
+).catch((err)=>{console.log(err)});
 
 app.listen(3000, ()=>{
     console.log('listening on port 3000');
