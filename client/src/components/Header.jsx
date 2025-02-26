03b6ac662e9fe6fbb08018ad1fe2925e234fc4ff
Naman Nayal <nayalnaman@gmail.com>
2025-02-26 15:44:16 +0530

    ui navbar fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,12 @@ import { Link, useLocation } from 'react-router-dom';
 export default function Header() {
     const path = useLocation().pathname;
   return (
-    <Navbar className='border-b-2'>
+    <Navbar className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
       <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+        <span className=' text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>
           Naman's
         </span>
         Blog
@@ -33,7 +33,8 @@ export default function Header() {
 
         </Button>
         <Link to='/sign-in'>
-          <Button >Sign In</Button>
+        <button className="relative inline-flex items-center justify-center p-0.5  me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"><span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent ">Sign In</span>
+        </button>
         </Link>
         <Navbar.Toggle/>
       </div>
