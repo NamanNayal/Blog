a238ca5a09a1659a6e5471cad47636427eb531bc
Naman Nayal <nayalnaman@gmail.com>
2025-03-01 21:38:50 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next)=>{
     try{
@@ -22,6 +23,34 @@ export const signup = async(req, res, next)=>{
     }catch(e){
         next(e);
     }
+};
 
+export const signin = async(req,res, next) => {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password || email === '' || password === ''){
+            return next(errorHandler(400, 'All fields are required'));
+        }  
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(400, 'Invalid Credentials'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid Credentials'));
+        }
+        const token = jwt.sign(
+            {
+                id: validUser._id,    
+            }, process.env.JWT_SECRET,{expiresIn:'1week'},
+        );
+        const{ password: pass, ...rest} = validUser._doc;
+        res.status(200).cookie('acess_token', token, {
+            httpOnly: true,
+        }).json(rest); 
+
+    }catch(e){
+
+    }
 
 }
