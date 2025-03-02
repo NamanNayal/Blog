7feca1317ae17c1354ea7e3b2895081ae9c248f0
Naman Nayal <nayalnaman@gmail.com>
2025-03-02 23:01:00 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from  './user/userSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
 
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version : 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+});
 
+export const persistor = persistStore(store);
 
-//to create a store configureStore is a function from Redux Toolkit that simplifies store creation.It automatically sets up Redux DevTools and middleware like redux-thunk.
-// The store is the object that brings actions and reducers together. It holds the application state and allows you to dispatch actions.
-// The store is created by passing a reducer to the createStore function from Redux.
-// A reducer is a pure function in Redux that takes the current state and an action and returns a new state.
-//A reducer is a pure function in Redux that takes the current state and an action and returns a new state. A reducer is a function, but Redux needs an object when handling multiple slices of state.
-//
