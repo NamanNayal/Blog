2258c3f8e70aca1c30aa8b9afbcb41b563b21354
Naman Nayal <nayalnaman@gmail.com>
2025-03-16 21:47:22 +0530

    added dashboard

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 export default function ThemeProvider({children}) {
     const {theme} = useSelector(state=>state.theme);
   return (
-    <div className={theme}>
+    <div className={`${theme} transition-colors duration-300 ease-in-out`}>
         <div className={theme === "dark" ? "dark" : ""}>
        {children}
        </div>
