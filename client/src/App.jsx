ec401dbc2819049cc10413ab2370f6e63a45cbda
Naman Nayal <nayalnaman@gmail.com>
2025-08-04 23:49:07 +0530

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
-import SignIn from '../../SignIn';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import Footer from './components/Footer'
